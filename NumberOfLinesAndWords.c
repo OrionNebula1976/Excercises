ade0635b9c64aaa5eefc33a77a3462257b145e66
OrionNebula1976 <mihailfedotow@yandex.ru>
2016-09-26 22:17:19 +0300

    Add files via upload
    Строки считает, а вот слова нет. Не знаю почему.

---
+++ NumberOfLinesAndWords.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+int main () {
+    int c, nl, nw;
+    nl = nw = 0;
+    while ((c = getchar()) != EOF) {
+        if (c == '\n' ) {
+            ++nl;
+        }
+        if (c == " " || c == '\n' || c == '\t')
+            ++nw;
+    }
+    printf ("%d %d\n", nl, nw);
+    return 0;
+}
