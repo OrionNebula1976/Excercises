274afc406cc86f0906da0ca46478fbc77ba852a3
OrionNebula1976 <mihailfedotow@yandex.ru>
2016-09-29 18:06:47 +0300

    Add files via upload

---
+++ myenv.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+
+extern char ** environ;
+
+int main (){
+int i;
+char *user = "username"; //вместо username вставить реальное имя пользователя, возвращенное командой whoami//
+for (i=0; i<1000; i++){
+if ((strstr (environ[i], user)) != NULL){
+printf ("%s\n", environ[i]);}
+}
+return 0;
+}
+
+   
