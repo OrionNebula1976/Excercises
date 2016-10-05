c9030cf913103a1a8fc526a73bb66ad4820300c3
OrionNebula1976 <mihailfedotow@yandex.ru>
2016-10-06 02:16:11 +0400

    Update myenv.c

--- myenv.c
+++ myenv.c
@@ -3,10 +3,10 @@
 #include <unistd.h>
 #include <string.h>
 extern char ** environ;
-int main (){
+int main () {
 	int i;
 	char *user = "username"; //вместо username вставить реальное имя пользователя, возвращенное командой whoami
-	for ( i = 0; i < 1000; i ++ ){
+	for ( i = 0; i < 1000; i ++ ) {
 		if ((strstr (environ[i], user)) != NULL) {
 			printf ("%s\n", environ[i]);
 		}
