905ed9eadc2d12dc1ad1bb57165c148f86f7f143
OrionNebula1976 <mihailfedotow@yandex.ru>
2016-10-06 02:15:41 +0400

    Update myenv.c

--- myenv.c
+++ myenv.c
@@ -2,17 +2,16 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <string.h>
-
 extern char ** environ;
-
 int main (){
-int i;
-char *user = "username"; //вместо username вставить реальное имя пользователя, возвращенное командой whoami//
-for (i=0; i<1000; i++){
-if ((strstr (environ[i], user)) != NULL){
-printf ("%s\n", environ[i]);}
-}
-return 0;
+	int i;
+	char *user = "username"; //вместо username вставить реальное имя пользователя, возвращенное командой whoami
+	for ( i = 0; i < 1000; i ++ ){
+		if ((strstr (environ[i], user)) != NULL) {
+			printf ("%s\n", environ[i]);
+		}
+	}
+	return 0;
 }
 
    
